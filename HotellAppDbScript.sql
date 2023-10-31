00f060aa1141ef7b819ee95134842bf3bc2fe821
Dark <turkmvc@gmail.com>
2023-10-31 22:08:21 +0300

    Create HotellAppDbScript.sql

---
+++ HotellAppDbScript.sql
@@ -0,0 +1,359 @@
+USE [master]
+GO
+/****** Object:  Database [HotelDb]    Script Date: 31.10.2023 22:07:16 ******/
+CREATE DATABASE [HotelDb]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'HotelDb', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\HotelDb.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'HotelDb_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\HotelDb_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [HotelDb] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [HotelDb].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [HotelDb] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [HotelDb] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [HotelDb] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [HotelDb] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [HotelDb] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [HotelDb] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [HotelDb] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [HotelDb] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [HotelDb] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [HotelDb] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [HotelDb] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [HotelDb] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [HotelDb] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [HotelDb] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [HotelDb] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [HotelDb] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [HotelDb] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [HotelDb] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [HotelDb] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [HotelDb] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [HotelDb] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [HotelDb] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [HotelDb] SET RECOVERY FULL 
+GO
+ALTER DATABASE [HotelDb] SET  MULTI_USER 
+GO
+ALTER DATABASE [HotelDb] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [HotelDb] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [HotelDb] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [HotelDb] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [HotelDb] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [HotelDb] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'HotelDb', N'ON'
+GO
+ALTER DATABASE [HotelDb] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [HotelDb] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [HotelDb]
+GO
+/****** Object:  Table [dbo].[Cities]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cities](
+	[Id] [int] NOT NULL,
+	[CountryId] [tinyint] NULL,
+	[Name] [nvarchar](255) NOT NULL,
+	[Latitude] [float] NULL,
+	[Longitude] [float] NULL,
+	[Climate] [nvarchar](max) NULL,
+	[Economy] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Continents]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Continents](
+	[Id] [tinyint] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Description] [varchar](max) NULL,
+ CONSTRAINT [PK_Continents] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Countries]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Countries](
+	[Id] [tinyint] IDENTITY(1,1) NOT NULL,
+	[ContinentId] [tinyint] NOT NULL,
+	[Flag] [varchar](50) NOT NULL,
+	[PhoneCode] [varchar](20) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Description] [varchar](max) NULL,
+ CONSTRAINT [PK_Countries] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[Id] [int] NOT NULL,
+	[FirstName] [nvarchar](255) NOT NULL,
+	[LastName] [nvarchar](255) NOT NULL,
+	[IdCardNumber] [nvarchar](20) NOT NULL,
+	[ContinentId] [tinyint] NULL,
+	[CountryId] [tinyint] NULL,
+	[CityId] [int] NULL,
+	[DistrictId] [int] NULL,
+	[PhoneNumber] [nvarchar](20) NOT NULL,
+	[DateOfBirth] [date] NULL,
+	[Gender] [bit] NULL,
+	[IsMarried] [bit] NULL,
+	[CompanyName] [nvarchar](255) NULL,
+	[IsActive] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Districts]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Districts](
+	[Id] [int] NOT NULL,
+	[CityId] [int] NULL,
+	[PostCode] [nvarchar](10) NOT NULL,
+	[Name] [nvarchar](255) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Employees]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Employees](
+	[Id] [int] NOT NULL,
+	[FirstName] [nvarchar](255) NOT NULL,
+	[LastName] [nvarchar](255) NOT NULL,
+	[IdCardNumber] [nvarchar](20) NOT NULL,
+	[ContinentId] [tinyint] NULL,
+	[CountryId] [tinyint] NULL,
+	[CityId] [int] NULL,
+	[DistrictId] [int] NULL,
+	[UserName] [nvarchar](255) NOT NULL,
+	[Password] [nvarchar](255) NOT NULL,
+	[PhoneNumber] [nvarchar](20) NOT NULL,
+	[DateOfBirth] [date] NULL,
+	[StartDateOfWork] [date] NULL,
+	[Gender] [bit] NULL,
+	[IsMarried] [bit] NULL,
+	[Salary] [decimal](18, 2) NULL,
+	[IsActive] [bit] NULL,
+	[Address] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](255) NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[Amount] [float] NOT NULL,
+	[CategoryId] [int] NULL,
+	[IsActive] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Purchases]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Purchases](
+	[Id] [uniqueidentifier] NOT NULL,
+	[EmployeeId] [int] NOT NULL,
+	[SupplierId] [int] NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[Amount] [float] NOT NULL,
+	[PurchasedDate] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Rooms]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Rooms](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](255) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	[FloorNumber] [tinyint] NULL,
+	[Capacity] [tinyint] NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[IsActive] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[SaleDetails]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SaleDetails](
+	[Id] [uniqueidentifier] NOT NULL,
+	[SaleId] [uniqueidentifier] NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[Amount] [float] NOT NULL,
+	[Discount] [float] NOT NULL,
+	[TotalPrice] [decimal](18, 2) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Sales]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Sales](
+	[Id] [uniqueidentifier] NOT NULL,
+	[EmployeeId] [int] NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[RoomId] [int] NOT NULL,
+	[SaledDate] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Suppliers]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Suppliers](
+	[Id] [int] NOT NULL,
+	[FirstName] [nvarchar](255) NOT NULL,
+	[LastName] [nvarchar](255) NOT NULL,
+	[CompanyName] [nvarchar](255) NULL,
+	[Mail] [nvarchar](255) NULL,
+	[Address] [nvarchar](max) NULL,
+	[IsActive] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[TillActivities]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TillActivities](
+	[Id] [uniqueidentifier] NOT NULL,
+	[TillId] [tinyint] NOT NULL,
+	[InOrOut] [char](1) NOT NULL,
+	[Amount] [decimal](18, 2) NOT NULL,
+	[Date] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tills]    Script Date: 31.10.2023 22:07:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tills](
+	[Id] [tinyint] NOT NULL,
+	[PaymentType] [nvarchar](255) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	[IsActive] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [HotelDb] SET  READ_WRITE 
+GO
